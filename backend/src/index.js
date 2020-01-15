c13ff323e8c812c016bd6b7760d147c86b8e5286
feijaogreg <feijaogreg@gmail.com>
2020-01-15 12:44:09 -0300

    First Commit

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require('mongoose');
+const routes = require('./routes')
+
+const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:F31j@099!@cluster0-qejpp.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+app.use(express.json()); // use diz que será valida para toda a plicação
+app.use(routes);
+
+// Tipos de parâmetros:
+
+// Query Params: req.query (Filtros, ordenação, paginação, ...)
+// Route Params: request.params (Identificar um recurso na alteração ou remoção)
+// Body: request.body (Dados para criação ou alteração de um registro)
+
+// MongoDB (Não-relacional)
+
+app.listen(3333);
