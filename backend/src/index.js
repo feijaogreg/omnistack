883d2d87dcaebad4c4e2d2927603f624958cfade
Matheus F <feijaogreg@gmail.com>
2020-01-17 15:33:43 -0300

    Modularization

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes')
 
 const app = express();
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://omnistack:F31j@099!@cluster0-qejpp.mongodb.net/w
     useUnifiedTopology: true
 })
 
+app.use(cors())
 app.use(express.json()); // use diz que será valida para toda a plicação
 app.use(routes);
 
--- web/src/App.js
+++ web/src/App.js
@@ -1,117 +1,49 @@
 import React, { useState, useEffect } from 'react';
+import api from './services/api';
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
 // Componente = um bloco isolado de html, css e js que nao interfere no restante da aplcação.
 // Propriedade = informações que um componente PAI passa para o componente FILHO
 // Estado = informações mantidas pelo componente (Lembrar: imutabilidade)
 
 
 function App() {
-  const [github_username, setGithub_username] = useState('');
-  const [techs, setTechs] = useState('');
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
+  const [devs, setDevs] = useState([]);
+
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        console.log(position)
-        const { latitude, longitude } = position.coords;
+    async function loadDevs() {
+      const response = await api.get('/devs');
 
-        setLatitude(latitude);
-        setLongitude(longitude);
-      },
-      (err) => {
-        console.log(err)
-      },
-      {
-        time: 30000
-      }
-    )
-    
+      setDevs(response.data);
+    }
+
+    loadDevs();
   }, [])
+
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data)
+    
+    setDevs([...devs, response.data]); //adição de elemento num array no reactjs
+
+  }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input onChange={e => setLatitude(e.target.value)} type="numeric" name="latitude" id="latitude" required value={latitude} />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input onChange={e => setLongitude(e.target.value)} type="numeric" name="longitude" id="longitude" required value={longitude} />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
+
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/2254731?s=400&v=4" alt=""/>
-              <div className="user-info">
-                <strong>Diego Fernandes</strong>
-                <span>ReactJS, React Native, NodeJS</span>
-              </div>
-            </header>
-            <p>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</p>
-            <a href="https://github.com/diego3g">Acessar perfil no GitHub</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/2254731?s=400&v=4" alt=""/>
-              <div className="user-info">
-                <strong>Diego Fernandes</strong>
-                <span>ReactJS, React Native, NodeJS</span>
-              </div>
-            </header>
-            <p>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</p>
-            <a href="https://github.com/diego3g">Acessar perfil no GitHub</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/2254731?s=400&v=4" alt=""/>
-              <div className="user-info">
-                <strong>Diego Fernandes</strong>
-                <span>ReactJS, React Native, NodeJS</span>
-              </div>
-            </header>
-            <p>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</p>
-            <a href="https://github.com/diego3g">Acessar perfil no GitHub</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/2254731?s=400&v=4" alt=""/>
-              <div className="user-info">
-                <strong>Diego Fernandes</strong>
-                <span>ReactJS, React Native, NodeJS</span>
-              </div>
-            </header>
-            <p>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</p>
-            <a href="https://github.com/diego3g">Acessar perfil no GitHub</a>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev.id} dev={dev} />
+          ))}
         </ul>
       </main>
     </div>
